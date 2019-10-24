0c3347bdf0402f88b65f3e4cb86ebcdf3f7e5498
Double J <doublej@Doubleui-MacBook-Pro.local>
2019-10-24 09:00:18 +0900

    [삭제] pystudy파일 이관으로 삭제 10.24

--- 2.py
+++
@@ -1,6 +0,0 @@
-print(10 + 5)
-import math
-print(math.ceil(2.2))
-print(math.floor(2.7))
-print(math.pow(2,30))
-print(math.pi)
--- 2.rb
+++
@@ -1,5 +0,0 @@
-puts(10 + 5)
-puts( 2.2.ceil() )
-puts( 2.7.floor() )
-puts( 2**30 )
-puts( Math::PI )
