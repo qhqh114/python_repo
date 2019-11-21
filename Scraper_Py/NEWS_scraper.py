d5de39c6ca8d5afcb2c487c009f62ebe85cb5c5f
qhqh114 <45251557+qhqh114@users.noreply.github.com>
2019-11-21 17:40:31 +0900

    Delete NEWS_scraper.py

--- Scraper_Py/NEWS_scraper.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-from datetime import datetime
-RESULT_PATH = 'D:/python study/beautifulSoup_ws/crawling_result/'
-now = datetime.now() #파일이름 현 시간으로 저장하기
-
